4167f35b47b2abeb78006515f5ce5e1a4ad3c554
Jaime Martinez Rincon <mrjaime1999@gmail.com>
2016-04-13 17:54:11 +0200

    Version 1.5.7
    Added maven to the project and (hopefully) fixed a exception when using paperspigot

---
+++ src/main/java/me/jaime29010/combatactionbar/MainCommand.java
@@ -0,0 +1,28 @@
+package me.jaime29010.combatactionbar;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class MainCommand implements CommandExecutor {
+    private final Main main;
+
+    public MainCommand(Main main) {
+        this.main = main;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (sender instanceof Player) {
+            Player player = (Player) sender;
+            main.reloadConfig();
+            main.enablePlugin();
+            player.sendMessage(ChatColor.GREEN + "The plugin has been reloaded");
+        } else {
+            sender.sendMessage("This command can only be executed by a player");
+        }
+        return true;
+    }
+}
---
+++ src/main/java/me/jaime29010/combatactionbar/SoundInfo.java
@@ -0,0 +1,31 @@
+package me.jaime29010.combatactionbar;
+
+import org.bukkit.Sound;
+import org.bukkit.entity.Player;
+
+public final class SoundInfo {
+    private final Sound sound;
+    private final float volume, pitch;
+
+    public SoundInfo(Sound sound, float volume, float pitch) {
+        this.sound = sound;
+        this.volume = volume;
+        this.pitch = pitch;
+    }
+
+    public Sound getSound() {
+        return sound;
+    }
+
+    public float getVolume() {
+        return volume;
+    }
+
+    public float getPitch() {
+        return pitch;
+    }
+
+    public void play(Player player) {
+        player.playSound(player.getLocation(), sound, volume, pitch);
+    }
+}
